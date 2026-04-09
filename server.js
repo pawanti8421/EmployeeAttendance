609645efae5d47e9ecd93f485723977656ef7567
Pawan Tiwari <pawantiwari8421@gmail.com>
2026-04-09 22:35:35 +0530

    done

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ connectDB();
 // ── Middleware ────────────────────────────────
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://employeeattandance.onrender.com",
+    credentials: true,
+  }),
+);
 
 app.use(
   session({
